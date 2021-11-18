224739a75086c87e3972183e959557047ea44fd7
Omar Al Haj <omaralhaj24@gmail.com>
2021-11-18 11:01:58 +0200

    DataManager interface

--- src/DataManager.java
+++ src/DataManager.java
@@ -1,2 +1,13 @@
+import java.util.List;
+
 public interface DataManager {
+    public Boolean isAvailable(String userName);
+    public List<Driver> listAllPendingDrivers();
+    public User getUser(String userName);
+    public Boolean addToPassenger(Passenger passenger);
+    public Passenger getPassenger(String userName);
+    public Boolean addToDriver(Driver driver);
+    public Driver getDriver(String userName);
+    public Boolean addToArea(Area area);
+    public Area getArea(String location);
 }
