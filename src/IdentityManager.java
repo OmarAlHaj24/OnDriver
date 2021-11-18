f96e28d91d869da0b69afed695364aea5ba04c05
Alaa228 <alaamahmoud.1223@gmail.com>
2021-11-18 11:03:50 +0200

    identityManager

--- src/IdentityManager.java
+++ src/IdentityManager.java
@@ -1,2 +1,25 @@
 public class IdentityManager {
+    User currentUser;
+
+    public void registerAsPassenger(Passenger user){
+
+    }
+
+    public void registerAsDriver(Driver user){
+
+    }
+
+    public Boolean loginAsPassenger(String username, String password){
+
+        return null;
+    }
+
+    public Boolean loginAsDriver(String username, String password){
+        return null;
+    }
+
+    public Boolean loginAsAdmin(String username, String password){
+        return null;
+    }
+
 }
