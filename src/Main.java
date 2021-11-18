98e121f953399c5901120c18ad4cbf010648a0c9
Omar Al Haj <omaralhaj24@gmail.com>
2021-11-18 15:35:58 +0200

    added the one and only admin that rules the system

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        ListManager manager = ListManager.getInstance();
+        Admin admin = new Admin("admin", "010", "admin@gmail.com", "admin");
+        manager.addToAdmin(admin);
         Menu menu = new Menu();
         menu.startMenu();
     }
