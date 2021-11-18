db9a8b8af005bbf6bb4066652d7ebab2e27ed1ed
Alaa228 <alaamahmoud.1223@gmail.com>
2021-11-18 12:39:12 +0200

    .

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,15 +1,37 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Passenger extends User{
-    private List<Ride>pastRides;
+    private ArrayList<Ride>pastRides;
     private Ride currentRide;
+    private Passenger(){
+        pastRides = new ArrayList<Ride>();
+    }
+
+    public void setPastRides(ArrayList<Ride> pastRides) {
+        this.pastRides=pastRides;
+    }
+    public List<Ride> getPastRides() {
+        return pastRides;
+    }
+
+    public void setCurrentRide(Ride currentRide) {
+        this.currentRide=currentRide;
+    }
+    public Ride getCurrentRide() {
+        return currentRide;
+    }
 
     public void requestRide(Ride ride){
-
+        currentRide = ride;
     }
 
     public void rateRide(Ride ride, int rate){
-
+        for (int i=0;i<pastRides.size ();i++){
+            if (pastRides.get (i).equals (ride)){
+                pastRides.get (i)
+            }
+        }
     }
 
     public double getRating(String username){
@@ -26,10 +48,13 @@ public class Passenger extends User{
     }
 
     public void listPastRides(){
+        for (int i=0;i<pastRides.size ();i++){
+            System.out.println (pastRides.get (i));
+        }
 
     }
 
     public Ride getPastRide(int index){
-        return null;
+        return pastRides.get (index);
     }
 }
