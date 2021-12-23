697795850335accbeed6c26f1bfce017945729de
AlaaElattar <alaamahmoud.1223@gmail.com>
2021-12-23 23:33:25 +0200

    ..

--- src/main/java/com/API/OnDriver/PassengerController.java
+++ src/main/java/com/API/OnDriver/PassengerController.java
@@ -10,50 +10,61 @@ public class PassengerController {
     private Ride currentRide;
     private ArrayList <Ride> pastRides;
 
+    @PostMapping("/passenger/request/{currentUsername}")
+    public String requestRide(@RequestBody Ride ride,@PathVariable String currentUsername) {
+        Passenger passenger = IdentityManager.getPassenger ( currentUsername );
+        if (passenger==null){
+            return "You have no access to this functionality!!";
+        }
 
-    @PostMapping("/passenger/request")
-    public String requestRide(@RequestBody Ride ride) {
-        ride.getSource().notifyDrivers(ride);
-        currentRide = ride;
+        passenger.requestRide (ride);
         return "Ride was requested successfully";
     }
 
-    @PostMapping("/passenger/rateRide/{rate}")
-    public int rateRide(@RequestBody Ride ride, @PathVariable int rate) {
-        if(rate<1 || rate > 5){
+    @PostMapping("/passenger/rateRide/{currentUsername}/{rate}")
+    public String rateRide(@RequestBody Ride ride, @PathVariable int rate, @PathVariable String currentUsername) {
+        Passenger passenger = IdentityManager.getPassenger ( currentUsername );
+        if (passenger==null){
+            return "You have no access to this functionality!!";
+        }
+        if (rate <0 || rate>5 ){
+            return "Wrong enter of rating!";
+        }
+        passenger.rateRide ( ride,rate );
+        return "ride was successfuly rated";
+    }
+
+
+    @GetMapping("/passenger/viewOffers/{currentUsername}")
+    public int viewOffers(@PathVariable String currentUsername) {
+        Passenger passenger = IdentityManager.getPassenger ( currentUsername );
+        if (passenger==null){
             return -1;
+        } else{
+            passenger.checkOffers ();
+            return 1;
         }
-        for (int i = 0; i < pastRides.size(); i++) {
-            if (pastRides.get(i).equals(ride)) { //We may need to overload the "equals" function later on..."
-                pastRides.get(i).getAcceptedOffer().getDriver().getRating().addRating(ride, rate);
-            }
-        }
-        return 0;
     }
 
-    @GetMapping("/passenger/checkOffers")
-    public Ride checkOffers() {
-        return currentRide.viewOffers();
+    @PostMapping("/passenger/acceptOffers/{currentUsername}/{offerNum}")
+    public String acceptOffer(@PathVariable String currentUsername, @PathVariable int offerNum) {
+        Passenger passenger = IdentityManager.getPassenger ( currentUsername );
+        if (passenger==null){
+            return "You have no access to this functionality!!";
+        }
+        if (passenger.acceptOffer ( offerNum )){
+            return "Offer is accepted successfully";
+        };
+        return "Offer is not accepted !!";
     }
 
-    @PostMapping("/passenger/acceptOffers/{offerNum}")
-    public String acceptOffer(@PathVariable int offerNum) {
-        if(offerNum >= currentRide.getOffers().size() || offerNum < 0){
-            return "Failed to accept offer !!";
+    @PostMapping("/passenger/viewPastRides/{currentUsername}")
+    public String listPastRides(@PathVariable String currentUsername) {
+        Passenger passenger = IdentityManager.getPassenger ( currentUsername );
+        if (passenger==null){
+            return "You have no access to this functionality!!";
         }
-        Offer accepted = currentRide.getOffers().get(offerNum);
-        currentRide.setAcceptedOffer(accepted);
-        pastRides.add(currentRide);
-        currentRide = null;
-        return "Offer was accepted successfully";
-    }
-
-    @PostMapping("/passenger/viewPastRides")
-    public String listPastRides() {
-        boolean flag = false;
-        for (int i = 0; i < pastRides.size(); i++) {
-            return pastRides.get ( i ).toStringPassenger ();
-        }
+        passenger.listPastRides ();
         return "All Past rides viewed successfully";
     }
 
