f6f9d081b7f309ff643f0a27ff93f5c60b75cae6
Ranaihab <rana.ihab7@gmail.com>
2021-12-23 18:09:31 +0200

    .

--- src/AreaSubject.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public interface AreaSubject {
-    public void subscribe(Driver driver);
-
-    public void unsubscribe(Driver driver);
-
-    public void notifyDrivers(Ride ride);
-}
--- src/UserStatus.java
+++
@@ -1,5 +0,0 @@
-public enum UserStatus {
-    suspended,
-    activated,
-    admin
-}
