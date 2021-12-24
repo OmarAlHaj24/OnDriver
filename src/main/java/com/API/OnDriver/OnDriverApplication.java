06e28b8c95f6fdcc758872971a684c93da9f8242
Omar Al Haj <omaralhaj24@gmail.com>
2021-12-24 12:42:30 +0200

    ..

--- src/main/java/com/API/OnDriver/OnDriverApplication.java
+++ src/main/java/com/API/OnDriver/OnDriverApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnDriverApplication {
 
-	public static void main(String[] args) {
-		ListManager manager = ListManager.getInstance();
-		Admin admin = new Admin("admin", "010", "admin@gmail.com", "admin");
-		manager.addToAdmin(admin);
-		SpringApplication.run(OnDriverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ListManager manager = ListManager.getInstance();
+        Admin admin = new Admin("admin", "010", "admin@gmail.com", "admin");
+        manager.addToAdmin(admin);
+        SpringApplication.run(OnDriverApplication.class, args);
+    }
 
 }
